985831c060f1441af26dce962e3779d0da4a409d
EmmanuelMitchell <emmanuelmitchell1998@gmail.com>
2024-11-13 14:28:35 +0000

    feat: change the enviroment variable

--- frontend/src/pages/utils/config.js
+++ frontend/src/pages/utils/config.js
@@ -1,5 +1,5 @@
 const config = {
-  BASE_URL: "http://localhost:3000", // Update this to your actual backend URL or IP address if testing on mobile
+  BASE_URL: import.meta.env.VITE_BASE_URL, // Update this to your actual backend URL or IP address if testing on mobile
   GOOGLE_DRIVE_URL:
     "https://drive.google.com/file/d/1poiQWBIr3ZMPBLPdQ-kMj3jkHEiuVr9I/view?usp=drivesdk",
   TESTFLIGHT_URL: "https://testflight.apple.com/join/TyjwXqWn",
